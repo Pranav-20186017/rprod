d3bd78c7dbfc5f4ff834eab862ce3ff852051cbe
Pranav Surampudi <sharmapranav307@msitprogram.net>
2020-02-05 22:12:30 +0530

    added node install script

---
+++ inode.sh
@@ -0,0 +1,6 @@
+apt update
+apt -y upgrade
+apt update
+apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+apt -y install nodejs
